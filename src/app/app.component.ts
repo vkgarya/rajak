12524068238c34bb7913efae600dc3755439158e
vkgarya <vkg.arya@gmail.com>
2021-05-29 22:22:23 +0530

    text file and json file generated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { saveAs } from 'file-saver';
+import { Observable, of } from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,7 @@ export class AppComponent implements OnInit {
     location: 'pune'
   }
   fileName = '';
+
   ngOnInit() {
     if (this.dataObject.ename === 'rajak') {
       this.fileName = 'test-template.log';
@@ -24,16 +26,67 @@ export class AppComponent implements OnInit {
     }
   }
 
-  createFile(){
-    // var FileSaver = require('file-saver');
-    // var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
-    // FileSaver.saveAs(blob, "hello world.txt");
-
-    let fileData = [];
-    fileData.push(JSON.stringify(this.dataObject));
-
-    var FileSaver = require('file-saver');
-    var blob = new Blob(fileData, {type: "text/plain;charset=utf-8"});
-    FileSaver.saveAs(blob, this.fileName);
+  createFile() {
+    saveAs(this.dataObject, this.fileName);
   }
+
+  fakeValidateUserData() {
+    return of(this.dataObject);
+  }
+
+  private setting = {
+    element: {
+      dynamicDownload: null as HTMLElement
+    }
+  }
+
+  dynamicDownloadTxt() {
+    this.fakeValidateUserData().subscribe((res) => {
+      this.dyanmicDownloadByHtmlTag({
+        fileName: this.fileName,
+        text: JSON.stringify(res)
+      });
+    });
+
+  }
+
+  dynamicDownloadJson() {
+    this.fakeValidateUserData().subscribe((res) => {
+      this.dyanmicDownloadByHtmlTag({
+        fileName: this.fileName,
+        text: JSON.stringify(res)
+      });
+    });
+  }
+
+  private dyanmicDownloadByHtmlTag(arg: {
+    fileName: string,
+    text: string
+  }) {
+    if (!this.setting.element.dynamicDownload) {
+      this.setting.element.dynamicDownload = document.createElement('a');
+    }
+    const element = this.setting.element.dynamicDownload;
+    const fileType = arg.fileName.indexOf('.json') > -1 ? 'text/json' : 'text/plain';
+    element.setAttribute('href', `data:${fileType};charset=utf-8,${encodeURIComponent(arg.text)}`);
+    element.setAttribute('download', arg.fileName);
+
+    var event = new MouseEvent("click");
+    element.dispatchEvent(event);
+  }
+
+  // reference : https://stackoverflow.com/a/51806624/7291379
+  // author : UnluckyAj
+  //  private dyanmicDownloadByBlob(arg: {
+  //   fileName: string,
+  //   text: string
+  // }) {
+  //     const fileType = arg.fileName.indexOf('.json') > -1 ? 'application/json' : 'text/plain';
+  //   var blob = new Blob([arg.text], { type: fileType });
+  //   var url = window.URL.createObjectURL(blob);
+  //   var pwa = window.open(url);
+  //   if (!pwa || pwa.closed || typeof pwa.closed == 'undefined') {
+  //       alert('Please disable your Pop-up blocker and try again.');
+  //   }
+  // }
 }
